fde713c4396a927a89dc925f691fc540e36f6523
TheGrimJam <gavin.mccormack@gmail.com>
2022-05-04 22:50:42 +0100

    Replaces User with UserResponse object in registration, and shorthands errors to prevent death by brackets

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,7 +1,7 @@
 import { MyContext } from "src/types";
 import { User } from "../entities/User"
 import { Arg, Ctx, Field, InputType, Mutation, ObjectType, Resolver } from "type-graphql";
-import argon, { argon2id } from "argon2"
+import argon from "argon2"
 
 @InputType()
 class UsernamePasswordInput {
@@ -25,22 +25,38 @@ class UserResponse {
     @Field(() => [FieldError], {nullable: true})
     errors?: FieldError[]
 
-    @Field(() => User)//, { nullable: true })
+    @Field(() => User, {nullable: true})
     user?: User
 }
 
+function createFieldError(field: string, message: string): [FieldError]{
+    return [{
+        field: field,
+        message: message
+    }]
+}
 
 @Resolver()
 export class UserResolver {
-    @Mutation(() => User)
+    @Mutation(() => UserResponse)
     async register(
         @Arg('options') options: UsernamePasswordInput,
         @Ctx() {em}: MyContext
-    ) {
+    ): Promise<UserResponse> {
+        if (options.username.length <= 2) {
+            return { 
+                errors: createFieldError('username', 'Username is too short')
+            }
+        }
+        if (options.password.length <= 3) {
+            return {
+                errors: createFieldError('password', 'Password is too short')
+            }
+        }
         const hashedPassword = await argon.hash(options.password)
         const user = em.create(User, {username: options.username, password: hashedPassword})
         await em.persistAndFlush(user)
-        return user
+        return {user: user}
     }
 
     @Mutation(() => UserResponse)
@@ -51,20 +67,13 @@ export class UserResolver {
         const user = await em.findOne(User, {username: options.username})
         if (!user) {
             return {
-                errors: [{
-                    field: "username",
-                    message: "that username doesn't exist"
-                }
-            ]
-            };
+                errors: createFieldError('username', 'Username does not exist')
+            }
         }
         const valid = await argon.verify(user.password, options.password)
         if (!valid) {
-            return { 
-                errors: [{
-                    field: "password",
-                    message: "Invalid password"
-                }]
+            return {
+                errors: createFieldError('password', 'Incorrect Password')
             }
         }
 
